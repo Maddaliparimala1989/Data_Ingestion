f3282ffe61ede879eb54a5988329f31611ba3498
Parimala <parimalamaddali89@gmail.com>
2024-03-02 16:55:58 -0500

    Added function to provide specific month

--- src/utilities.py
+++ src/utilities.py
@@ -4,11 +4,19 @@ import calendar
 
 
 def provide_calendar(year: int):
-    y = 2024
+    y = year
     for m in range(1, 13):
         print(calendar.month(y, m))
     return True
 
 
+def provide_month(year: int, month: int):
+    y = year
+    m = month
+    print(calendar.month(y, m))
+    return True
+
+
 if __name__ == '__main__':
-    provide_calendar(2024)
+    # provide_calendar(2024)
+    provide_month(2022, 6)
